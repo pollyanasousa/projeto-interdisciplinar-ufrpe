f7142de351392a595ef1f00ceb97629efb660816
GS-bit <gabriel.soares.lx@gmail.com>
2026-04-01 13:41:21 -0300

    Estruturação inicial do projeto

---
+++ README.m
@@ -0,0 +1,8 @@
+Projeto em construção!
+
+Estrutura configurada conforme sugestão do professor:
+model/ -> guarda as classes do sistema
+utils/ -> terá funções auxiliares
+data/ -> terá os arquivos usados para armazenar dados (json ou txt)
+
+Podemos criar mais pastas, dependendo das necessidades do projeto.
