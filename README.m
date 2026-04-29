758ebf7ca13a27166bb4e5d19ef9a74acbf200f9
Pollyana Sousa <pollyanacassia8@gmail.com>
2026-04-29 19:25:34 -0300

    Removendo README.m antigo

--- README.m
+++
@@ -1,8 +0,0 @@
-Projeto em construção!
-
-Estrutura configurada conforme sugestão do professor:
-model/ -> guarda as classes do sistema
-utils/ -> terá funções auxiliares
-data/ -> terá os arquivos usados para armazenar dados (json ou txt)
-
-Podemos criar mais pastas, dependendo das necessidades do projeto.
