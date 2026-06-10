d4a02aac50d5e3b97673fae35925cea79cf4873b
GS-bit <gabriel.soares.lx@gmail.com>
2026-06-10 15:19:35 -0300

    GUI integration v9

--- gui/agrobook_window.py
+++ gui/agrobook_window.py
@@ -115,15 +115,19 @@ class AgroBookWindow(QMainWindow):
         self.expenses_screen.done_button.clicked.connect(self.events.process_expenses)
         self.expenses_screen.new_expense_button.clicked.connect(self.events.new_expense)
         self.expenses_screen.delete_expense_button.clicked.connect(self.events.delete_expense)
+        self.expenses_screen.update_expense_button.clicked.connect(self.events.update_expense)
 
         self.areas_screen.done_button.clicked.connect(self.events.process_areas)
         self.areas_screen.new_area_button.clicked.connect(self.events.new_area)
         self.areas_screen.delete_area_button.clicked.connect(self.events.delete_area)
+        self.areas_screen.update_area_button.clicked.connect(self.events.update_area)
 
         self.planting_screen.done_button.clicked.connect(self.events.process_planting)
         self.planting_screen.new_planting_button.clicked.connect(self.events.new_planting)
         self.planting_screen.delete_planting_button.clicked.connect(self.events.delete_planting)
+        self.planting_screen.update_planting_button.clicked.connect(self.events.update_planting)
 
         self.harvests_screen.done_button.clicked.connect(self.events.process_harvests)
         self.harvests_screen.new_harvest_button.clicked.connect(self.events.new_harvest)
         self.harvests_screen.delete_harvest_button.clicked.connect(self.events.delete_harvest)
+        self.harvests_screen.update_harvest_button.clicked.connect(self.events.update_harvest)
